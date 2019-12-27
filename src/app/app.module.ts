7922deecd2b4e556574a802d210dee12d924b308
SimpleCoreTechnologies <201153614@student.lbschools.net>
2019-12-27 15:28:04 -0800

    Started implementing the router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,17 @@ import { ArtistItemsComponent } from './artist-items/artist-items.component';
 import { SearchArtistsPipe } from './search-artists.pipe';
 import { ArtistDetailsComponent } from './artist-details/artist-details.component';
 import { WordMatchComponent } from './word-match/word-match.component';
+
+const routes = [
+  {
+    path: 'word',
+    component: WordMatchComponent
+  }
+  //{
+  //  path: 'directory',
+   // component:
+ // }
+]
 
 @NgModule({
   declarations: [
@@ -22,7 +34,8 @@ import { WordMatchComponent } from './word-match/word-match.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [
